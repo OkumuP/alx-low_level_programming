5a3b06d183038d24b841ccd47e1fd5bbddcb451e
OkumuP <okumupita@gmail.com>
2023-03-24 08:35:46 -0700

    A function that returns the sum of all its parameters updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,12 @@
-#ifndef_VARIADIC_H_
-#define_VARIADIC_H_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
-int_putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
