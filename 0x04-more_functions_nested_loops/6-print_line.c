abf1dd5e30a3c2c70fea7e8d1c3783aa959f0c5b
OkumuP <okumupita@gmail.com>
2023-03-31 10:26:07 -0700

    An update to more functions and nested loops directory creating two files

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * print_line -> printing line
- * @n: integer params
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
-	int x;
-
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-		for (x = 0; x < n; x++)
-		       _putchar(95);
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
+
 }	
