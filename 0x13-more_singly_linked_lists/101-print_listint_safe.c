67d278d81b936ea177b8688f96c866512f6f7db9
OkumuP <okumupita@gmail.com>
2023-04-05 05:18:26 -0700

    An update to the file 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,51 +6,52 @@ size_t print_listint_safe(const listint_t *head);
 
 /**
  * looped_listint_len - Counts the number of unique nodes
- * in a looped linked list
- * @head: A pointer to the head of the linked list to check.
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
  *
- * Return: the number of unique nodes
- * Or - 0 if the list is not looped
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
  */
 size_t looped_listint_len(const listint_t *head)
 {
-	const listint_t *sheep, *goat;
+	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	sheep = head->next;
-	goat = (head->next)->next;
+	tortoise = head->next;
+	hare = (head->next)->next;
 
 	while (hare)
 	{
-		if (sheep == goat)
+		if (tortoise == hare)
 		{
-			sheep = head;
-			while (sheep != goat)
+			tortoise = head;
+			while (tortoise != hare)
 			{
 				nodes++;
-				sheep = sheep->next;
-				goat = goat->next;
+				tortoise = tortoise->next;
+				hare = hare->next;
 			}
 
-			sheep  = sheep->next;
-			while (sheep != goat)
+			tortoise = tortoise->next;
+			while (tortoise != hare)
 			{
 				nodes++;
-				sheep = sheep->next;
+				tortoise = tortoise->next;
 			}
 
 			return (nodes);
 		}
 
-		sheep = sheep->next;
-		goat = (goat->next)->next;
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
 
 	return (0);
 }
+
 /**
  * print_listint_safe - Prints a listint_t list safely.
  * @head: A pointer to the head of the listint_t list.
