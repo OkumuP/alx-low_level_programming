64cc35f78ba904b613f94a2bee8e32f3e5fc314a
OkumuP <okumupita@gmail.com>
2023-06-21 13:14:26 -0700

    An update to 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,52 +7,55 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - program to return the sum of two numbers
- * @a: The first number.
- * @b: The second number.
+ * op_add - Returns the sum
+ * @a: The first number
+ * @b: The second number
  *
- * Return: The sum
+ * Return: Sum
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 /**
- * op_sub - program to return the difference between two numbers
- * @a: The first number.
- * @b: The second number.
- * Return: The difference 
+ * op_sub - Returns the difference
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Difference
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mul - program to return the product of two numbers
- * @a: The first number.
- * @b: The second number.
- * Return: The product
+ * op_mul - Returns the product
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Product
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
- * op_div - program to return the quotient of two numbers
+ * op_div - Returns the quotient
  * @a: The first number.
  * @b: The second number.
- * Return: The quotient
+ *
+ * Return: Quotient
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
- * op_mod - Returns the modulo o two numbers.
+ * op_mod - Returns the modulo
  * @a: The first number.
  * @b: The second number.
  *
- * Return: The modulo
+ * Return: Modulo
  */
 int op_mod(int a, int b)
 {
