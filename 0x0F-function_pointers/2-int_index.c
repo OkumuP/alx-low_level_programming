ac49feb8c5251971c0ab1f498df1cd5cb37032fb
OkumuP <okumupita@gmail.com>
2023-06-21 13:04:38 -0700

    An update to 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,22 +1,22 @@
 #include "function_pointers.h"
 /**
- * int_index - return index if true else -1
- * @array: the array
- * @size: the size of elements in an array
- * @cmp: pointer to function 
+ * int_index - return index if comparison = true, else -1
+ * @array: this is the array
+ * @size: size of array elements
+ * @cmp: pointer to one of the 3 functions in main
  * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int a;
+	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	for (a = 0; a < size; a++)
+	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[a]))
-			return (a);
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
