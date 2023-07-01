e153a8b021b203ab7dd52558e12124dc6a240b61
OkumuP <okumupita@gmail.com>
2023-07-01 11:37:00 -0700

    An update to 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t sz = 0;
+	size_t z = 0;
 
 	while (h)
 	{
@@ -17,8 +17,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len1, h->str);
 		h = h->next;
-		sz++;
+		z++;
 	}
 
-	return (sz);
+	return (z);
 }
