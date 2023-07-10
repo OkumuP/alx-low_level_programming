fd6e1817d96ee8dc06f7594c11c509bffe4a65f7
OkumuP <okumupita@gmail.com>
2023-07-10 07:24:49 -0700

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,18 +10,18 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	ssize_t tr;
-	ssize_t wr;
-	ssize_t s;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	tr = open(filename, O_RDONLY);
-	if (tr == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	s = read(fd, buf, letters);
-	wr = write(STDOUT_FILENO, buf, s);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
-	close(tr);
-	return (wr);
+	close(fd);
+	return (w);
 }
